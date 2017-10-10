2ab1d831178310b8dabc072c2c04555e88176862
FadedLocks702 <31429405+FadedLocks702@users.noreply.github.com>
2017-10-09 23:33:43 -0700

    day 1 outline
    not even pseudocode yet -_-.. gonna attempt NY commands

---
+++ starter_code/js/script.js
@@ -0,0 +1,28 @@
+//User enters values "New York", "New York City" or "NYC"
+//then make the background of the page nyc.jpg
+//	user input New York
+//	click on submit button
+//	background changes to nyc.jpg
+//	continue default
+
+
+
+
+
+
+
+
+// "San Francisco" or "SF" or "Bay Area"
+//then make the background of the page sf.jpg
+
+
+//Los Angeles" or "LA" or "LAX"
+//then make the background of the page la.jpg
+
+
+//"Austin" or "ATX"
+//then make the background of the page austin.jpg
+
+
+//"Sydney" or "SYD" 
+//then make the background of the page sydney.jpg
