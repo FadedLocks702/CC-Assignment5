7b68effe62d862e207f72fb29a79a34014f1af7e
FadedLocks702 <31429405+FadedLocks702@users.noreply.github.com>
2017-10-10 21:32:05 -0700

    started over
    couldn't pseudocode until i at least get one of the values right.. only took 3 hours -_-

--- starter_code/js/script.js
+++ starter_code/js/script.js
@@ -1,28 +1,48 @@
-//User enters values "New York", "New York City" or "NYC"
-//then make the background of the page nyc.jpg
-//	user input New York
-//	click on submit button
-//	background changes to nyc.jpg
-//	continue default
+
+
+$(document).ready(function(){
+// Call $.val() on inputs to get the string value of your user's input
+
+$("#submit-btn").click(changeBackground);
+
+function changeBackground(evt){
+	evt.preventDefault();
+	var f = $('#city-type').val();
+	alert(f);
+
+	if (f == "NYC"){
+		$("body").attr("class","nyc");
+	}
+}
+
+
+//Store user input in var city
+
+//Use .submit() or $.click to figure out when the user clicks the "submit" button
+
+//Create if / else if / else conditionals to control the flow of your application
+
+//Write at least six different lines of pseudocode and 
+//display them inline as JavaScript comments
+
+//Prevent a page refresh using the evt.preventDefault() function
+
+//Use the $.ready() handler to delay your code from executing 
+//until all DOM assets have been loaded
+
+//Get the first element from an attribute name using $.attr()
+
+//Use the || operator in your conditionals to allow for 
+//multiple string values to execute if/else if statement code
+});
 
 
 
+//If a user submits:
 
-
-
-
-
-// "San Francisco" or "SF" or "Bay Area"
-//then make the background of the page sf.jpg
-
-
-//Los Angeles" or "LA" or "LAX"
-//then make the background of the page la.jpg
-
-
-//"Austin" or "ATX"
-//then make the background of the page austin.jpg
-
-
-//"Sydney" or "SYD" 
-//then make the background of the page sydney.jpg
+// "New York" or "New York City" or "NYC" make the background of the page nyc.jpg
+// "San Francisco" or "SF" or "Bay Area" make the background of the page sf.jpg
+// "Los Angeles" or "LA" or "LAX" make the background of the page la.jpg
+// "Austin" or "ATX" make the background of the page austin.jpg
+// "Sydney" or "SYD" make the background of the page sydney.jpg
+// Take a look at what your final product should look like before and after:
